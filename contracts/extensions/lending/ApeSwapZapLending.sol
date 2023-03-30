3d6332e2484a38a0f0e4a42e25d859cecc7d189a
defifofum <defifofum0@gmail.com>
2023-03-30 15:40:06 -0400

    refactor: ApeSwapZap Lending, condense functions

--- contracts/extensions/lending/ApeSwapZapLending.sol
+++ contracts/extensions/lending/ApeSwapZapLending.sol
@@ -8,11 +8,10 @@ abstract contract ApeSwapZapLending is ApeSwapZap {
     using SafeERC20 for IERC20;
     using SafeERC20 for ICErc20;
 
-    event ZapLending(IERC20 inputToken, uint256 inputAmount, ICErc20 market);
-    event ZapLendingNative(uint256 inputAmount, ICErc20 market);
+    event ZapLendingMarket(IERC20 inputToken, uint256 inputAmount, ICErc20 market, uint256 outputAmount);
 
     /// @dev Native token market underlying
-    address public constant NATIVE_UNDERLYING = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address public constant LENDING_NATIVE_UNDERLYING = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
     constructor() {}
 
@@ -31,18 +30,9 @@ abstract contract ApeSwapZapLending is ApeSwapZap {
         uint256 deadline,
         ICErc20 market
     ) external nonReentrant {
-        _validateLendingMarketZap(inputToken, path, market);
-
-        uint256 balanceBefore = _getBalance(inputToken);
-        inputToken.safeTransferFrom(msg.sender, address(this), inputAmount);
-        inputAmount = _getBalance(inputToken) - balanceBefore;
-
-        inputToken.approve(address(router), inputAmount);
-        _routerSwap(inputAmount, minAmountsSwap, path, deadline);
-        (, uint256 cTokensReceived) = _mintLendingMarket(market);
+        inputAmount = _transferIn(inputToken, inputAmount);
+        uint256 cTokensReceived = _zapLendingMarket(inputToken, inputAmount, path, minAmountsSwap, deadline, market);
         market.transfer(msg.sender, cTokensReceived);
-
-        emit ZapLending(inputToken, inputAmount, market);
     }
 
     /// @notice Zap native token to a Lending Market
@@ -56,35 +46,50 @@ abstract contract ApeSwapZapLending is ApeSwapZap {
         uint256 deadline,
         ICErc20 market
     ) external payable nonReentrant {
-        (uint256 inputAmount, IERC20 inputToken) = _wrapNative();
-        _validateLendingMarketZap(inputToken, path, market);
-
-        inputToken.approve(address(router), inputAmount);
-        _routerSwap(inputAmount, minAmountsSwap, path, deadline);
-        (, uint256 cTokensReceived) = _mintLendingMarket(market);
+        (IERC20 weth, uint256 inputAmount) = _wrapNative();
+        uint256 cTokensReceived = _zapLendingMarket(weth, inputAmount, path, minAmountsSwap, deadline, market);
         market.transfer(msg.sender, cTokensReceived);
-
-        emit ZapLendingNative(inputAmount, market);
     }
 
-    function _validateLendingMarketZap(
+    /** INTERNAL FUNCTIONS **/
+
+    /// @notice Zap token single asset lending market
+    /// @param inputToken Input token to zap
+    /// @param inputAmount Amount of input tokens to zap
+    /// @param path Path from input token to stake token
+    /// @param minAmountsSwap The minimum amount of output tokens that must be received for swap
+    /// @param deadline Unix timestamp after which the transaction will revert
+    /// @param market Lending market to deposit to
+    function _zapLendingMarket(
         IERC20 inputToken,
+        uint256 inputAmount,
         address[] calldata path,
+        uint256 minAmountsSwap,
+        uint256 deadline,
         ICErc20 market
-    ) private view returns (IERC20 underlyingToken) {
-        underlyingToken = IERC20(market.underlying());
-        if (address(underlyingToken) == NATIVE_UNDERLYING) {
-            underlyingToken = IERC20(WNATIVE);
+    ) internal returns (uint256 cTokensReceived) {
+        // Validate path and market underlying
+        IERC20 underlyingToken = IERC20(market.underlying());
+        if (address(underlyingToken) == LENDING_NATIVE_UNDERLYING) {
+            /// @dev The lending market uses a special address to represent native token
+            underlyingToken = IERC20(address(WNATIVE));
         }
         require(
             (address(inputToken) == path[0] && address(underlyingToken) == path[path.length - 1]),
             "ApeSwapZapLending: Wrong path for inputToken or principalToken"
         );
+
+        _routerSwap(inputAmount, minAmountsSwap, path, deadline, true);
+        (, cTokensReceived) = _mintLendingMarket(market);
+
+        emit ZapLendingMarket(inputToken, inputAmount, market, cTokensReceived);
     }
+
+    /** PRIVATE FUNCTIONS **/
 
     function _mintLendingMarket(ICErc20 market) private returns (uint256 depositAmount, uint256 cTokensReceived) {
         IERC20 underlyingToken = IERC20(market.underlying());
-        if (underlyingToken == IERC20(WNATIVE) || address(underlyingToken) == NATIVE_UNDERLYING) {
+        if (underlyingToken == IERC20(address(WNATIVE)) || address(underlyingToken) == LENDING_NATIVE_UNDERLYING) {
             depositAmount = _unwrapNative();
             market.mint{value: depositAmount}();
         } else {
