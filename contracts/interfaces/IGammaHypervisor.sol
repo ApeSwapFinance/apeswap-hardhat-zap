3034be99599d90e3e2068c335d6f4f03ad8a9bec
Doublo <doublo54@gmail.com>
2023-05-25 18:54:33 +0200

    feat: Added zapping into gamma liquidity

---
+++ contracts/interfaces/IGammaHypervisor.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.15;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface Hypervisor {
+    function token0() external view returns (IERC20);
+
+    function token1() external view returns (IERC20);
+
+    function whitelistedAddress() external view returns (address);
+
+    function withdraw(
+        uint256 shares,
+        address to,
+        address from,
+        uint256[4] memory minAmounts
+    ) external returns (uint256 amount0, uint256 amount1);
+}
